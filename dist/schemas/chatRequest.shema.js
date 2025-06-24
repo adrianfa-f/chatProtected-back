f4dae96b95a3ac62ced9bb7f796361b1efdce44a
adrianfa-f <adrianfrancoavila1995@gmail.com>
2025-06-23 20:38:44 -0400

    pues no

--- dist/schemas/chatRequest.shema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createRequestShema = void 0;
-const zod_1 = require("zod");
-exports.createRequestShema = zod_1.z.object({
-    toUserId: zod_1.z.string()
-});
