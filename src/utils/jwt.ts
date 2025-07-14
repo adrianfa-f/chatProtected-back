f11779c91801fed034ab511c9e53ce8a1f8c24e0
adrianfa-f <adrianfrancoavila1995@gmail.com>
2025-07-14 17:08:21 -0400

    token perpetuo

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,17 +2,16 @@ import jwt from 'jsonwebtoken';
 import { Request } from 'express';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'secret_key';
-const TOKEN_EXPIRATION = '1d';
 
 export const generateToken = (userId: string) => {
-    return jwt.sign({ userId }, JWT_SECRET, { expiresIn: TOKEN_EXPIRATION });
+    return jwt.sign({ userId }, JWT_SECRET); // Eliminado expiresIn
 };
 
 export const verifyToken = (token: string) => {
     try {
         return jwt.verify(token, JWT_SECRET) as { userId: string };
     } catch (error) {
-        console.log("Este es el error que estamos recibiendo: ", error)
-        throw new Error('Token inválido o expirado');
+        console.log("Error verificando token:", error);
+        throw new Error('Token inválido');
     }
 };
